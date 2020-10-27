e5d34009ac1c31266625bc19a6bc755dae2fbb77
zhouyouwei <122613761@qq.com>
2020-10-27 23:47:54 +0800

    提交稳重分拣系统app，基于javafx开发，jdk1.8

--- README.md
+++
@@ -1,1 +0,0 @@
-# wegther-plus-app
--- src/test/java/com.codetratise/BusinessTest.java
+++ src/test/java/com.codetratise/BusinessTest.java
@@ -1,6 +1,8 @@
 package com.codetratise;
 
 import com.codetreatise.util.HttpClient;
+import com.codetreatise.util.SerialUtils.ParamConfig;
+import com.codetreatise.util.SerialUtils.SerialPortUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -137,5 +139,36 @@ public class BusinessTest {
         Map result = HttpClient.doPost(url, paras);
         System.out.println("6    " + result);
     }
+
+    /**
+     * 八、串口读取测试
+     *
+     * 参考文章 https://www.cnblogs.com/new-life/p/9345849.html
+     *
+     * @throws Exception
+     */
+    @Test
+    public void serialPortUtilsest() {
+
+        try {
+
+            // 实例化串口操作类对象
+            SerialPortUtils serialPort = new SerialPortUtils();
+            // 创建串口必要参数接收类并赋值，赋值串口号，波特率，校验位，数据位，停止位
+            ParamConfig paramConfig = new ParamConfig("COM4", 9600, 0, 8, 1);
+            // 初始化设置,打开串口，开始监听读取串口数据
+            serialPort.init(paramConfig);
+            // 调用串口操作类的sendComm方法发送数据到串口
+            serialPort.sendComm("FEF10A000000000000000AFF");
+            // 关闭串口
+            serialPort.closeSerialPort();
+        } catch (Exception e) {
+
+            System.out.println(e.fillInStackTrace());
+        }
+
+
+    }
+
 
 }
